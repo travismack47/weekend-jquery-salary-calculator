5974c6574b08ffcd43cb39803a982aa79214ccbe
Travis Mack <travismack47@protonmail.com>
2023-05-06 12:33:56 -0500

    added function to append new table row with input values when submit button is cliced

--- client.js
+++ client.js
@@ -2,4 +2,28 @@ $(onReady)
 
 function onReady() {
     console.log('hello');
+
+    $('#submitButton').on('click', addEmployeeData);
+    $('.employeeSubmit').on('click', '.deleteButton', deleteButton);
+}
+
+
+
+
+function addEmployeeData() {
+    $('.employeeSubmit').append(`
+    <tr>
+    <td>${$("#employeeFirstName").val()}</td>
+    <td>${$("#employeeLastName").val()}</td>
+    <td>${$("#employeeIDNumber").val()}</td>
+    <td>${$("#employeeJobTitle").val()}</td>
+    <td>${$("#employeeAnnualSalary").val()}</td>
+    <td><button class="deleteButton">Delete</button></td>
+    </tr>
+    `)
+}
+
+
+function deleteButton(event) {
+$(event.target).closest('tr').remove();
 }
